74587758bb0824b8b5a5169802b2f02836535b3a
PiotrLas <piotr.las@protonmail.com>
2023-10-04 23:11:32 +0200

    add new fields to item in database

--- api/schemas.py
+++ api/schemas.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel
 class ItemDB(BaseModel):
     id: int
     name: str
+    weapon: str
+    stattrak: bool
+    quality: str
     sell_listings: int
     sell_price: int
     icon_url: str
--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 url = "https://steamcommunity.com/market/search/render/"
@@ -28,36 +30,65 @@ async def get_data_json(
 # import json
 #
 #
-# with open("api/data.json", "r") as f:
+# with open("data.json", "r") as f:
 #     data = json.load(f)
-
-# print(data)
 
 
 async def filter_data(data: dict, filter_keys: list):
+    """
+    Filter data from json
+    :param data: Data from json
+    :param filter_keys: List of keys to filter from data
+    :return: Dict containing keys from filter_keys and their values
+    """
     filtered_data = []
     item_keys = []
     item_description_keys = []
     for key in filter_keys:
         if key in data["results"][0].keys():
+            if key == "name":
+                continue
             item_keys.append(key)
         else:
             item_description_keys.append(key)
 
-    # print(item_keys, item_description_keys)
-
+    # print(item_keys)
     for result in data["results"]:
         # desc_dict = {}
         # for key in item_description_keys:
         #     desc_dict[key] = result["asset_description"][key]
 
         filtered_data.append(
-            {key: result[key] for key in item_keys}
+            await split_name(result["name"])
+            | {key: result[key] for key in item_keys}
             | {key: result["asset_description"][key] for key in item_description_keys}
         )
-        # print(desc_dict)
+        # print(filtered_data)
 
     return filtered_data
+
+
+async def split_name(full_name: str):
+    """
+    Split name into name, weapon, StatTrak, quality
+    :param full_name: Full name of item
+    :return: {name: name, weapon: weapon, stattrak: stattrak, quality: quality}
+    """
+    if "|" not in full_name:
+        return {
+            "name": full_name,
+            "weapon": "Invalid",
+            "stattrak": False,
+            "quality": "Invalid",
+        }
+    weapon, name = full_name.strip().split(" | ")
+    stattrak = False
+    if "StatTrak" in weapon:
+        weapon = weapon[10:]
+        stattrak = True
+    name, quality = name.split(" (")
+    quality = quality[:-1]
+    return {"name": name, "weapon": weapon, "stattrak": stattrak, "quality": quality}
 
 
 # db_columns = ["name", "sell_listings", "sell_price", "icon_url", "appid"]
