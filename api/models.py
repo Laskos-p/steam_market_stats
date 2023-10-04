284af5b74f7f9d01b6c7f0aa6c51ec4f036b8f08
PiotrLas <116374910+PiotrLas@users.noreply.github.com>
2023-10-04 23:17:33 +0200

    Update models.py
    remove unused imports

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-
 from sqlalchemy import Boolean, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
