7e75892d0ada2ac39b4c897cf8bebc6b971409f7
PiotrLas <piotr.las@protonmail.com>
2023-10-17 22:54:28 +0200

    update database: - add alphabetical_order column - add is_listed column

--- api/models.py
+++ api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, ForeignKey, Integer, String
+from sqlalchemy import Boolean, ForeignKey, Identity, Integer, String
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
 from sqlalchemy.sql import func
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -18,6 +18,9 @@ class Item(Base):
     __tablename__ = "items"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    alphabetical_order: Mapped[int] = mapped_column(
+        Integer, Identity(start=1), unique=True
+    )
     full_name: Mapped[str] = mapped_column(String, unique=True)
     name: Mapped[str] = mapped_column(String)
     weapon: Mapped[str] = mapped_column(String)
@@ -26,6 +29,7 @@ class Item(Base):
     sell_listings: Mapped[int] = mapped_column(Integer)
     sell_price: Mapped[int] = mapped_column(Integer)
     icon_url: Mapped[str] = mapped_column(String)
+    is_listed: Mapped[bool] = mapped_column(Boolean, default=True)
 
     appid: Mapped[int] = mapped_column(
         Integer, ForeignKey("games.appid", ondelete="CASCADE"), nullable=False
