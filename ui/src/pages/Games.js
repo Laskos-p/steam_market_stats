b7ed957b680b167c6af287790a2a191423ad547d
PiotrLas <piotr.las@protonmail.com>
2023-10-20 15:02:49 +0200

    added simple front-end view of games in db

--- ui/src/pages/Games.js
+++ ui/src/pages/Games.js
@@ -1,3 +1,43 @@
+import {useEffect, useState} from "react";
+
 export default function Games() {
-    return <h1>Games</h1>
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/games/", {method: "GET"})
+            .then(
+                response => {
+                    if (response.ok) {
+                        return response.json()
+                    }
+                    throw response;
+                }, error => {
+                    console.log(error);
+                }
+            )
+            .then(
+                data => {
+                    setData(data);
+                }
+            )
+    }, []);
+     return (
+        <>
+            <h1>Games</h1>
+            <ul className="games">
+                {data && data.map(game => (
+                    <li key={crypto.randomUUID()}>
+                        <img
+                            src={game.header_image}
+                            alt="logo"
+                        />
+                        <div className="game">
+                            <div className="game-name">
+                                {game.name}
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
