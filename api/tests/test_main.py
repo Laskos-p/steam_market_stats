2ddfddb65340fe5606e54d8b90011aac57b8ea36
piotr <piotr.las@protonmail.com>
2023-11-02 13:58:41 +0100

    added simple test file

---
+++ api/tests/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from api.main import app
+
+client = TestClient(app)
+
+
+# test if the main endpoint is working
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
