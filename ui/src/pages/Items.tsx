c80217dcb8ad93e6d773084cafb6e1e3350e1e20
A-Wodnicki <wodnicki.adrian4@gmail.com>
2024-02-26 15:42:42 +0100

    Refactor Items component to use TypeScript and handle errors

--- ui/src/pages/Items.tsx
+++ ui/src/pages/Items.tsx
@@ -1,71 +1,77 @@
 import { useEffect, useState } from "react";
-import React from "react";
+
+interface Item {
+  id: number;
+  full_name: string;
+  name: string;
+  weapon: string;
+  stattrak: boolean;
+  quality: string;
+  sell_listings: number;
+  sell_price: number;
+  icon_url: string;
+  appid: number;
+}
 
 export default function Items() {
-  const [data, setData] = useState<Array<unknown>>([]);
+  const [data, setData] = useState<Item[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  async function fetchItems() {
+    const response = await fetch("http://127.0.0.1:8080/items");
+    return (await response.json()) as Item[];
+  }
+
   useEffect(() => {
-    fetch("http://127.0.0.1:8080/items", { method: "GET" })
-      .then(
-        (response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw response;
-        },
-        (error) => {
-          console.log(error);
-        },
-      )
-      .then((data) => {
-        setData(data);
+    fetchItems()
+      .then((data) => setData(data))
+      .catch((error: Error) => {
+        setError(error.message);
       });
   }, []);
+
   return (
     <>
       <h1>Items</h1>
-      <ul className="items">
-        {data &&
-          data.map((item: unknown) => (
-            <li key={crypto.randomUUID()}>
-              <img
-                src={
-                  "https://community.akamai.steamstatic.com/economy/image/" +
-                  (item as { icon_url: string }).icon_url
-                }
-                alt="logo"
-              />
-              <div className="item-info">
-                <div className="item-name">
-                  {(item as { full_name: string }).full_name}
+      {error || (
+        <ul className="items">
+          {data &&
+            data.map((item: Item) => (
+              <li key={crypto.randomUUID()}>
+                <img
+                  src={
+                    "https://community.akamai.steamstatic.com/economy/image/" +
+                    item.icon_url
+                  }
+                  alt="logo"
+                />
+                <div className="item-info">
+                  <div className="item-name">{item.full_name}</div>
+                  <div className="item-price-info">
+                    <span className="item-price-label">Price</span>
+                    <span className="item-price">
+                      {"$" + item.sell_price / 100 + " USD"}
+                    </span>
+                  </div>
+                  <div className="item-quantity-info">
+                    <span className="item-quality-label">Quantity</span>
+                    <span className="item-quantity">{item.sell_listings}</span>
+                  </div>
+                  <div className="link-to-steam">
+                    <a
+                      href={
+                        "https://steamcommunity.com/market/listings/730/" +
+                        item.full_name
+                      }
+                    >
+                      Link to item on steam
+                    </a>
+                  </div>
                 </div>
-                <div className="item-price-info">
-                  <span className="item-price-label">Price</span>
-                  <span className="item-price">
-                    {"$" +
-                      (item as { sell_price: number }).sell_price / 100 +
-                      " USD"}
-                  </span>
-                </div>
-                <div className="item-quantity-info">
-                  <span className="item-quality-label">Quantity</span>
-                  <span className="item-quantity">
-                    {(item as { sell_listings: number }).sell_listings}
-                  </span>
-                </div>
-                <div className="link-to-steam">
-                  <a
-                    href={
-                      "https://steamcommunity.com/market/listings/730/" +
-                      (item as { full_name: string }).full_name
-                    }
-                  >
-                    Link to item on steam
-                  </a>
-                </div>
-              </div>
-            </li>
-          ))}
-      </ul>
+              </li>
+            ))}
+        </ul>
+      )}
     </>
   );
 }
