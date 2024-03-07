109127e764421279a0e3fd843a5c7f4357a98907
A-Wodnicki <116439661+A-Wodnicki@users.noreply.github.com>
2024-03-07 22:59:08 +0100

    Fix item story to use the actual component

--- ui/src/components/stories/ItemList.stories.tsx
+++ ui/src/components/stories/ItemList.stories.tsx
@@ -1,23 +1,22 @@
 import { StoryObj, Meta, composeStories } from "@storybook/react";
 import * as stories from "./Item.stories";
+import ItemList from "../ItemList";
+import ItemInfo from "../../lib/types/ItemInfo";
 
 const { DefaultItem } = composeStories(stories);
 
 export default {
   title: "ItemList",
+  component: ItemList,
   tags: ["autodocs"],
-} satisfies Meta;
+  args: {
+    data: Array.from({ length: 5 }, (_, i) => ({
+      ...(DefaultItem.args as ItemInfo),
+      id: i,
+    })),
+  },
+} satisfies Meta<typeof ItemList>;
 
-type Story = StoryObj;
+type Story = StoryObj<typeof ItemList>;
 
-export const Default: Story = {
-  render: () => (
-    <ul className="space-y-4">
-      {Array.from({ length: 4 }).map(() => (
-        <li key={crypto.randomUUID()}>
-          <DefaultItem />
-        </li>
-      ))}
-    </ul>
-  ),
-};
+export const Default: Story = {};
